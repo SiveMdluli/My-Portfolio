696467a82f5787dd6fa452f1b029251aa31acce9
Sive Mdluli <mdlulisive202@gmail.com>
2023-04-15 09:38:45 +0200

    Add popup window, using dynamic javascript

--- script.js
+++ script.js
@@ -8,6 +8,133 @@ function closeNav(close) {
   return close;
 }
 
+const popup = document.getElementById('popupCard');
+const seebtn = document.getElementById('seeButton');
+const span = document.getElementsByClassName('close')[0];
+
+const popup1 = document.getElementById('popupCard1');
+const seebtn1 = document.getElementById('seeButton1');
+const span1 = document.getElementsByClassName('close1')[0];
+
+const popup2 = document.getElementById('popupCard2');
+const seebtn2 = document.getElementById('seeButton2');
+const span2 = document.getElementsByClassName('close2')[0];
+
+const popup3 = document.getElementById('popupCard3');
+const seebtn3 = document.getElementById('seeButton3');
+const span3 = document.getElementsByClassName('close3')[0];
+
+// POPUP WINDOW
+const popups = [{
+  card: 1,
+  name: 'Tonic',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  image: 'img/Snapshoot Portfolio desktop.png',
+  technologies: ['html', 'JavaScript', 'css'],
+  source: 'https://github.com/SiveMdluli/',
+  link: 'https://sivemdluli.github.io/',
+},
+{
+  card: 2,
+  name: 'Multi-Post Stories ',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  image: 'img/Snapshoot Portfolio(1).png',
+  technologies: ['html', 'JavaScript', 'css'],
+  source: 'https://github.com/SiveMdluli/',
+  link: 'https://sivemdluli.github.io/',
+},
+{
+  card: 3,
+  name: 'Tonic',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essente',
+  image: 'img/Snapshoot Portfolio(2).png',
+  technologies: ['html', 'JavaScript', 'css'],
+  source: 'https://github.com/SiveMdluli/',
+  link: 'https://sivemdluli.github.io/',
+},
+{
+  card: 4,
+  name: 'Multi-Post Stories',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  image: 'img/Snapshoot Portfolio desktop 2.png',
+  technologies: ['html', 'JavaScript', 'css'],
+  source: 'https://github.com/SiveMdluli/',
+  link: 'https://sivemdluli.github.io/',
+},
+];
+
+// Trigger card 0
+seebtn.onclick = function () {
+  popup.style.display = 'block';
+};
+
+span.onclick = function () {
+  popup.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target === popup) {
+    popup.style.display = 'none';
+  }
+};
+
+// Trigger card 1
+seebtn1.onclick = function () {
+  popup1.style.display = 'block';
+};
+
+span1.onclick = function () {
+  popup1.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target === popup1) {
+    popup1.style.display = 'none';
+  }
+};
+// Trigger card 2
+seebtn2.onclick = function () {
+  popup2.style.display = 'block';
+};
+
+span2.onclick = function () {
+  popup2.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target === popup2) {
+    popup2.style.display = 'none';
+  }
+};
+// Trigger card 3
+
+seebtn3.onclick = function () {
+  popup3.style.display = 'block';
+};
+
+span3.onclick = function () {
+  popup3.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target === popup3) {
+    popup3.style.display = 'none';
+  }
+};
+
+// Window trigger onclick
+window.onclick = function (event) {
+  if (event.target === popups) {
+    popups.style.display = 'none';
+  }
+};
+
+
+
+
+
+
+
 // CONTACT VALIDATION
 
 const form = document.querySelector("form");
